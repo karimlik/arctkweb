bb474bcfa6451d095d774281c10efbf2a9a21d53
karimlik <okarimlikarim@gmail.com>
2023-02-20 20:52:16 +0400

    first commit

---
+++ arctkweb/js/attached-menu.js
@@ -0,0 +1,25 @@
+var overlay = document.getElementById('overlay');
+var closeMenu = document.getElementById('close-menu');
+
+document.getElementById('open-menu').addEventListener('click', function() {
+	overlay.classList.add('show-menu');
+});
+
+document.getElementById('close-menu').addEventListener('click', function() {
+	overlay.classList.remove('show-menu');
+});
+
+var h_hght = 0;
+var h_mrg = 0;
+$(function(){
+    $(window).scroll(function(){
+        var top = $(this).scrollTop();
+        var elem = $('#top_nav');
+        if (top+h_mrg < h_hght) {
+            elem.css('top', (h_hght-top));
+        }
+        else {
+            elem.css('top', h_mrg);
+        }
+    });
+});
